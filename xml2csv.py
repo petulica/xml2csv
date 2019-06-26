5f6ccb5886a6f350ecbabca3f4708abc1f21094e
Petra cernohlavkova <cernohlavkovapetra@centum.cz>
2019-06-26 20:08:30 +0200

    funkční hledání převedeno do funkce. vytvořena fce pro každou sbíranou hodnotu. v xml 395994 přidán další jazyk pro testování.

--- xml2csv.py
+++ xml2csv.py
@@ -1,5 +1,6 @@
 import os
 from xml.etree import ElementTree as et
+from collections import Counter
 
 base_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -9,9 +10,37 @@ tree = et.parse(xml_file)
 
 root = tree.getroot()
 
-fields = root.findall(".//{http://www.loc.gov/MARC21/slim}datafield[@tag='999']")
-for field in fields:
-	for subfield in field:
-		if subfield.attrib["code"] == "a":
+def identifier():
+	fields = root.findall(".//{http://www.loc.gov/MARC21/slim}controlfield[@tag='001']")
+	for field in fields:
+		print(field.text)
+
+def doc_type():
+	fields = root.findall(".//{http://www.loc.gov/MARC21/slim}datafield[@tag='980']")
+	for field in fields:
+		for subfield in field:
 			print(subfield.text)
 
+def langs():
+	fields = root.findall(".//{http://www.loc.gov/MARC21/slim}datafield[@tag='041']")
+	for field in fields:
+		for subfield in field:
+			#if subfield.attrib["code"] == "a" or "b":
+				print(subfield.text)
+def year():
+	fields = root.findall(".//{http://www.loc.gov/MARC21/slim}datafield[@tag='046']")
+	for field in fields:
+		for subfield in field:
+			if subfield.attrib["code"] == "k":
+				print(subfield.text)
+
+def projects():
+	fields = root.findall(".//{http://www.loc.gov/MARC21/slim}datafield[@tag='999']")
+	for field in fields:
+		for subfield in field:
+			if subfield.attrib["code"] == "a":
+				project_sum = sum(Counter("a").values())
+				#print(subfield.text)
+				print(project_sum)
+
+identifier(), doc_type(),langs(), year(), projects()
